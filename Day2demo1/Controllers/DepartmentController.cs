e2e757454dcb54e9997f14979eb2e5c2886ea877
geethasamynathan <geethasamynathan2011@gmail.com>
2021-12-17 16:33:36 +0530

    Mvc Demo1

---
+++ Day2demo1/Controllers/DepartmentController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Day2demo1.Models;
+
+namespace Day2demo1.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        List<Department> departmentList;
+        public DepartmentController()
+        {
+            departmentList = new List<Department>() { 
+            new Department{Id=101,Name="IT",Location="Bangalore" },
+            new Department {Id=102,Name="Transport",Location="Mysore"}
+            
+            };
+
+        }
+        // GET: Department
+        //public string Index()
+        //{
+        //    return "Hello world";
+        //}
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// example fro ViewData
+
+        public ActionResult Getdepartments()
+        {
+            //     ViewData["deptnames"] = "Information Technology";
+            List<string> departments = new List<string>();
+            departments.Add("IT");
+            departments.Add("HR");
+            departments.Add("Admin");
+            ViewData["depts"] = departments;
+            return View();
+        }
+
+        public ActionResult GetDepts()
+        {
+            /*   ViewBag.deptname = "Huma resource";
+             *   */
+            List<string> Gencs = new List<string>();
+            Gencs.Add("Vandana");
+            Gencs.Add("Amey");
+            Gencs.Add("Anvesh");
+            ViewBag.Gencs = Gencs;
+            return View();
+        }
+
+        public ActionResult GetAllDepartments()
+        {
+           
+            return View(departmentList);
+        }
+
+        public ActionResult GetAll()
+        {
+            ViewBag.alldepts = departmentList;
+            return View();
+        }
+    }
+}
